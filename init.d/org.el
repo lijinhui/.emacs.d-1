cad66db38a553a8e234d1822ff4da57a3bdbe075
Brady Love <bradyx.k.love@intel.com>
2012-10-12 08:05:44 -0700

    Added 01_proxy.el to .gitignore, added notes to org.el

--- init.d/org.el
+++ init.d/org.el
@@ -1,7 +1,9 @@
+;; Add more todo keywords to ORG mode
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)")
         (sequence "FEATURE(f)" "|" "COMPLETED(c)")
         (sequence "BUG(b)" "|" "FIXED(x)")
         (sequence "|" "CANCELED(a)")))
 
+;; Timestamp items when completed
 (setq org-log-done 'time)
