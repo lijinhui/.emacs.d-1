0ee02475e21c455e4a4797144e425b541cc7ad50
Brady Love <bradyx.k.love@intel.com>
2012-12-17 14:46:22 -0800

    Added some helpers for org mode to use my personal org file git repo

--- init.d/org.el
+++ init.d/org.el
@@ -1,3 +1,27 @@
+;; Quick way to access org files
+(defun org-open () "Open file from ~/Org" (interactive)
+  (ido-find-file-in-dir "~/Org"))
+(global-set-key (kbd "C-S-O") 'org-open)
+
+;; Push ~/Org to GitHub
+(defun org-push () "Push Git repo at ~/Org" (interactive)
+  (save-buffer)
+  (shell-command "cd $HOME/Org")
+  (shell-command "git add .; git com -am \"Updated Org Files\"")
+  (shell-command "git push origin master"))
+
+;; Pull ~/Org from GitHub
+(defun org-pull () "Push Git repo at ~/Org" (interactive)
+  (shell-command "cd $HOME/Org")
+  (shell-command "git pull origin master")
+  (revert-buffer (current-buffer)))
+
+;; Clone repo to ~/Org from Github
+(defun org-initialize () "Clone my Org repo" (interactive)
+  (shell-command "cd $HOME")
+  (shell-command "git clone git@github.com:bradylove/Org.git"))
+
+
 ;; Add more todo keywords to ORG mode
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)")
