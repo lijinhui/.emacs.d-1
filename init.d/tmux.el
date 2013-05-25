b3141f78d73cc3a79ae9085d870600511af21e02
Brady Love <love.brady@gmail.com>
2013-05-24 21:47:32 -0700

    Renamed tmux-pane-name to tmux-pane-number

--- init.d/tmux.el
+++ init.d/tmux.el
@@ -1,20 +1,20 @@
 (setq tmux-session-name 0)
 (setq tmux-window-name 1)
-(setq tmux-pane-name 1)
+(setq tmux-pane-number 1)
 
 (defun tmux-exec (command)
   "Execute command in tmux pane"
   (interactive)
   (shell-command
-   (format "tmux send-keys -t %s:%s.%s '%s' Enter" tmux-session-name tmux-window-name tmux-pane-name command)))
+   (format "tmux send-keys -t %s:%s.%s '%s' Enter" tmux-session-name tmux-window-name tmux-pane-number command)))
 
 (defun tmux-setup (x y z)
   "Setup global variables for tmux session, window, and pane"
   (interactive "sEnter tmux session name: \nsEnter tmux window name: \nsEnter tmux pane number: ")
   (setq tmux-session-name x)
   (setq tmux-window-name y)
-  (setq tmux-pane-name z)
-  (message "Tmux Setup, session name: %s, window name: %s, pane number: %s" tmux-session-name tmux-window-name tmux-pane-name))
+  (setq tmux-pane-number z)
+  (message "Tmux Setup, session name: %s, window name: %s, pane number: %s" tmux-session-name tmux-window-name tmux-pane-number))
 
 (defun zcukes () "Run Cucumber under Zeus" (interactive)
   (tmux-exec "zeus cucumber"))
